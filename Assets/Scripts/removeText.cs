38415bfeeeaf9eef9a41d8857ac6ee6963cc23ce
KNAXtryhaaard <max.meinel@gmx.net>
2021-02-28 23:32:37 +0100

    update lvl 2

---
+++ Assets/Scripts/removeText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class removeText : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            Debug.Log("time is up");
+        }
+    }
+}
